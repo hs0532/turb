ed93cfa34bda6ee1addf58cea4cba9f2021dd7ab
hs0532 <a554042469@163.com>
2017-10-09 14:26:59 +0800

    test

--- common/common-service/src/main/java/com/util/test.java
+++ common/common-service/src/main/java/com/util/test.java
@@ -1,6 +1,9 @@
 package com.util;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
@@ -8,62 +11,24 @@ import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
 public class test {
-	public static void main(String[] args) {
-FTPClient ftp = new FTPClient();
-		String filePath= "/2/2";
-		try {
-			int reply;
-			ftp.connect("47.95.120.182",21);// 连接FTP服务器
-			ftp.login("ftpUser", "1234");//登录
-			reply = ftp.getReplyCode();
-			
-			ftp.changeWorkingDirectory("/home/images");// 转移到FTP服务器目录
-			ftp.makeDirectory("pic/2017/09");
-			FTPFile[] fs = ftp.listFiles();
-			for (FTPFile ftpFile : fs) {
-				 if(ftpFile.isFile()){  
-	                   System.out.println(ftpFile.getName());
-	                }  else{
-	                	System.out.println(ftpFile.getName());
-	                }
-			}
-			if(!FTPReply.isPositiveCompletion(reply)){
-				ftp.disconnect();
-				
-			}
-			if(!ftp.changeWorkingDirectory("/home/iamges/pic"+"/2")){
-				//System.out.println("22222 ");
-				String[] dirs = filePath.split("/");
-				String temPath = "/home/iamges/pic";
-				for (String dir : dirs) {
-					if(null == dir || "".equals(dir)) continue;
-					temPath += "/"+dir;
-					if(!ftp.changeWorkingDirectory(temPath)){
-						if(!ftp.makeDirectory(temPath)){
-							System.out.println("在目标服务器上成功建立了文件夹: "+temPath);
-							
-						}else{
-							ftp.changeWorkingDirectory(temPath);
-						}
-						
-					}
-				}
-			}
-		
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			if(ftp.isConnected()){
-				try {
-					ftp.disconnect();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		
-	}
+	
+ 
+    public static int digui(int n){  
+        if(n==1||n==0){  
+            return n;  
+        }else{  
+            System.out.println("执行第" + n + "次");  
+            return n*digui(n-1);  
+        }  
+    }  
+	public static void main (String[] args){  
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");   
+		Calendar cl = Calendar.getInstance(); 
+		cl.setTime(new Date());
+		int N_year = cl.get(Calendar.YEAR);
+		int N_month = cl.get(Calendar.MONTH);
+		System.out.println(N_year+"年"+N_month+"月");
+		String a="我是好人";
+		System.out.println(a.length());
+	}  
 }
